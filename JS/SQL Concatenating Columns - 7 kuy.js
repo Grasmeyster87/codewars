710c1fe0848204cc2d7d777c30d554c978a50a89
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-06-29 23:27:42 +0300

    JS - Rank: 7 kuy Honor: 76

---
+++ JS/SQL Concatenating Columns - 7 kuy.js
@@ -0,0 +1,52 @@
+/*
+Given the table below:
+
+** names table schema **
+
+    id
+    prefix
+    first
+    last
+    suffix
+
+Your task is to use a select statement to return a single column table containing the full title of the person (concatenate all columns together except id), as follows:
+
+** output table schema **
+
+    title
+
+Don't forget to add spaces.
+//-----------------------------
+Приведенная ниже таблица:
+
+** схема таблицы имен **
+
+id
+префикс
+первый
+последний
+суффикс
+
+Ваша задача — использовать оператор select для возврата таблицы с одним столбцом, содержащей полное название человека (объедините все столбцы вместе, кроме id), следующим образом:
+
+** схема таблицы вывода **
+
+название
+
+Не забудьте добавить пробелы.
+*/
+
+// SELECT;
+// CONCAT(prefix, ' ', first, ' ', last, ' ', suffix) AS title;
+// FROM;
+// names;
+
+/*
+Mr. Renna Doyle V
+Mr. Shelton Fay DDS
+Rev. Branden Mohr Esq.
+Rev. Svetlana Hessel JD
+Amb. Rich Herzog Esq.
+Pres. Justa Cremin VM
+Pres. Chet Sauer Jr.
+*/
