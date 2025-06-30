29efd837eb022abd8e2d3aa3479c9be770deb48a
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-06-30 16:58:37 +0300

    JS - Rank:7 kyu Honor:78

---
+++ JS/Convert a Boolean to a String - 8 kuy.js
@@ -0,0 +1,18 @@
+/*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+//---------------------------------------------------------------------------------------
+
+Реализуйте функцию, которая преобразует заданное логическое значение в его строковое представление.
+
+Примечание: будут предоставлены только допустимые входные данные.
+
+
+*/
+const value = "hello";
+
+function booleanToString(b) {
+    return String(Boolean(b));
+}
+
