7dd95589c143e2a7a7a3acef2d5b153eba08d5c7
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2025-12-31 11:46:02 +0200

    JS - MakeUpperCase - 8 kuy Rank:4 kyu Honor:1,303

---
+++ JS/MakeUpperCase - 8 kuy/MakeUpperCase - 8 kuy.test.js
@@ -0,0 +1,11 @@
+const { assert } = require('chai');
+const makeUpperCase = require('./MakeUpperCase - 8 kuy');
+
+describe('Basic Tests', function () {
+    it('should pass the basic tests', function () {
+        assert.strictEqual(makeUpperCase(''), '');
+        assert.strictEqual(makeUpperCase('hello'), 'HELLO');
+        assert.strictEqual(makeUpperCase('Hello'), 'HELLO');
+        assert.strictEqual(makeUpperCase('HELLO'), 'HELLO');
+    });
+});
