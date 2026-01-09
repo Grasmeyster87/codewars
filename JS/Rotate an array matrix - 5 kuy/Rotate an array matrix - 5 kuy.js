3d7b5aba3cba2a73d5ef59067a2249a00c02a2c1
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-01-09 21:08:37 +0200

    JS - Rotate an array matrix - 5 kuy Rank:4 kyu Honor:1,398

--- JS/Rotate an array matrix - 5 kuy/Rotate an array matrix - 5 kuy.js
+++ JS/Rotate an array matrix - 5 kuy/Rotate an array matrix - 5 kuy.js
@@ -1,5 +1,20 @@
-
 function rotate(matrix, direction) {
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+
+    if (direction === 'clockwise') {
+        // Clockwise: нова матриця має розмір cols x rows
+        return Array.from({ length: cols }, (_, i) =>
+            Array.from({ length: rows }, (_, j) => matrix[rows - 1 - j][i])
+        );
+    } else if (direction === 'counter-clockwise') {
+        // Counter-clockwise: нова матриця має розмір cols x rows
+        return Array.from({ length: cols }, (_, i) =>
+            Array.from({ length: rows }, (_, j) => matrix[j][cols - 1 - i])
+        );
+    } else {
+        throw new Error('Invalid direction: use "clockwise" or "counter-clockwise"');
+    }
 }
 
-module.exports - rotate;
+module.exports = rotate;
