32f85f0750ee5617379ac33484f519ae0e59f7d2
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-01-13 18:35:49 +0200

    JS - Array Exchange - 6 kuy Rank:4 kyu Honor:1,422

--- JS/Array Exchange - 6 kuy/Array Exchange - 6 kuy.js
+++ JS/Array Exchange - 6 kuy/Array Exchange - 6 kuy.js
@@ -1,4 +1,23 @@
 function exchangeWith(a, b) {
-  // your code here
+  const aRev = [...a].reverse();
+  const bRev = [...b].reverse();
+  
+  a.length = 0;
+  b.length = 0;
+  
+  for (let i = 0; i < bRev.length; i++) {
+    a.push(bRev[i]);
+  }
+  
+  for (let i = 0; i < aRev.length; i++) {
+    b.push(aRev[i]);
+  }
+  
+  return [a, b];
 }
+
+const a = ['1', '2', '3', '4', '5', '6', '7'];
+const b = ['a', 'b', 'c'];
+
+console.log(exchangeWith(a, b));
 module.exports = exchangeWith;
