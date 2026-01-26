edd4a653970125571adda1dbb9d8b0b25f8f5c55
Grameyster87 <grasmeyster@gmail.com>
2026-01-26 08:25:07 +0200

    JS - Duplicate Encoder - 6 kuy Rank:4 kyu Honor:1,548

--- JS/Duplicate Encoder - 6 kuy/Duplicate Encoder - 6 kuy.js
+++ JS/Duplicate Encoder - 6 kuy/Duplicate Encoder - 6 kuy.js
@@ -1,25 +1,32 @@
-function duplicateEncode(word) {    
-    return word.toLowerCase().split('').map(
-        (el) =>{ 
-        let target = el;
-        let count = 0;
 
-        for (let i = 0; i < word.length; i++) {
-        if (word[i] === target) {
-         count++;
-        }
-        }
-        if (count % 2 == 0) {
-            el = ')'            
-        } else {
-            el = '('            
+function duplicateEncode(word) {
+  // приводим всё к нижнему регистру
+  const lower = word.toLowerCase();
 
-        }
-    }
-    );
+  return lower
+    .split('')
+    .map((ch, _, arr) => {
+      // считаем сколько раз символ встречается
+      const count = arr.filter(c => c === ch).length;
+      return count > 1 ? ')' : '(';
+    })
+    .join('');
 }
-let res = duplicateEncode('Success'); //")())())"
 
-console.log(res);
+/* function duplicateEncode(word) {
+  const lower = word.toLowerCase();
+  const freq = {};
+
+  // Считаем сколько раз встречается каждый символ
+  for (const ch of lower) {
+    freq[ch] = (freq[ch] || 0) + 1;
+  }
+
+  // Формируем строку скобок
+  return lower
+    .split('')
+    .map(ch => (freq[ch] > 1 ? ')' : '('))
+    .join('');
+} */
 
 module.exports = duplicateEncode;
--- JS/Duplicate Encoder - 6 kuy/Duplicate Encoder - 6 kuy.test.js
+++ JS/Duplicate Encoder - 6 kuy/Duplicate Encoder - 6 kuy.test.js
@@ -1,14 +1,16 @@
-const chai = require("chai");
-const assert = chai.assert;
-chai.config.truncateThreshold=0;
-const duplicateEncode = require("./Duplicate Encoder - 6 kuy");
+(async () => {
+  const chai = await import("chai");
+  const { assert } = chai;
+  chai.config.truncateThreshold = 0;
 
+  const duplicateEncode = require("./Duplicate Encoder - 6 kuy");
 
-describe("Duplicate Encoder", () => {
-  it("Testing for fixed tests", () => {
-    assert.strictEqual(duplicateEncode("din"),"(((");
-    assert.strictEqual(duplicateEncode("recede"),"()()()");
-    assert.strictEqual(duplicateEncode("Success"),")())())","should ignore case");
-    assert.strictEqual(duplicateEncode("(( @"),"))((");
+  describe("Duplicate Encoder", () => {
+    it("Testing for fixed tests", () => {
+      assert.strictEqual(duplicateEncode("din"), "(((");
+      assert.strictEqual(duplicateEncode("recede"), "()()()");
+      assert.strictEqual(duplicateEncode("Success"), ")())())", "should ignore case");
+      assert.strictEqual(duplicateEncode("(( @"), "))((");
+    });
   });
-});
+})();
