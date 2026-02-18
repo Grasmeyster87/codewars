e6479f624c782e45d022432e8c144a613abc3fca
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-02-18 06:41:22 +0200

    JS - Basic variable assignment - 8 kuy Rank:3 kyu Honor:1,822

---
+++ JS/Basic variable assignment - 8 kuy/Basic variable assignment - 8 kuy.test.js
@@ -0,0 +1,9 @@
+const { assert } = require("chai")
+const name = require('./Basic variable assignment - 8 kuy'); 
+
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(name, "codewa.rs");
+    })
+  })
