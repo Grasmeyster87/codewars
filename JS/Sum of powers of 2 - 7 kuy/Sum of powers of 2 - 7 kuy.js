f6c190246521ce16c2fef4c2ad236f89ccf683a0
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-02-27 08:21:50 +0200

    JS - Sum of powers of 2 - 7 kuy Rank:3 kyu Honor:1,873

--- JS/Sum of powers of 2 - 7 kuy/Sum of powers of 2 - 7 kuy.js
+++ JS/Sum of powers of 2 - 7 kuy/Sum of powers of 2 - 7 kuy.js
@@ -1,7 +1,16 @@
 // return an array of numbers (that are a power of 2)
 // for which the input "n" is the sum
 const powers = n => {
+  const result = [];
+  let power = 1;
 
+  while (power <= n) {
+    if (Math.floor(n / power) % 2 === 1) {
+      result.push(power);
+    }
+    power *= 2;
+  }
+  return result;
 };
 
 module.exports = powers;
