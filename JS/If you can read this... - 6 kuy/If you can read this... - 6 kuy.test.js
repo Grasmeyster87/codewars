2931c5a478811ee187f00e30578358e77810836a
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-07-16 22:06:58 +0300

    Change If you can read this... - 6 kuy

--- JS/If you can read this... - 6 kuy/If you can read this... - 6 kuy.test.js
+++ JS/If you can read this... - 6 kuy/If you can read this... - 6 kuy.test.js
@@ -1,12 +1,34 @@
+const chai = require('chai');
+const { strictEqual } = chai.assert; // Правильно дістаємо strictEqual
+
+// Словник NATO додано для того, щоб код міг працювати локально.
+// На самій платформі він вже існує глобально.
+const NATO = {
+    'A': 'Alfa', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo', 
+    'F': 'Foxtrot', 'G': 'Golf', 'H': 'Hotel', 'I': 'India', 'J': 'Juliett', 
+    'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November', 'O': 'Oscar', 
+    'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 'S': 'Sierra', 'T': 'Tango', 
+    'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey', 'X': 'Xray', 'Y': 'Yankee', 
+    'Z': 'Zulu'
+};
+
+function toNato(words) {
+    return words
+        .replace(/\s/g, '')
+        .toUpperCase()
+        .split('')
+        .map(char => NATO[char] || char)
+        .join(' ');
+}
+
+// Блок тестів
 describe('Tests', function () {
-    const chai = require('chai'),
-        { strictEqual } = chai.assert;
     chai.config.truncateThreshold = 0;
-    const toNato = require('./Integer complexity - 5 kuy');
 
     function doTest(input, expected) {
         const actual = toNato(input);
-        assert.strictEqual(actual, expected, `for string = "${input}"\n`);
+        // Використовуємо strictEqual напряму, оскільки ми його імпортували[cite: 3]
+        strictEqual(actual, expected, `for string = "${input}"\n`);
     }
 
     it('Sample Tests', function () {
@@ -18,6 +40,9 @@ describe('Tests', function () {
             'Did not see that coming',
             'Delta India Delta November Oscar Tango Sierra Echo Echo Tango Hotel Alfa Tango Charlie Oscar Mike India November Golf',
         );
-        doTest('go for it!', 'Golf Oscar Foxtrot Oscar Romeo India Tango !');
+        doTest(
+            'go for it!', 
+            'Golf Oscar Foxtrot Oscar Romeo India Tango !'
+        );
     });
 });
