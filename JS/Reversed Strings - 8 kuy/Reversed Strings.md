c3a51f94c43143be1f2ce4915962a74e0692fc6d
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2026-08-10 22:54:39 +0300

    JS - Reversed Strings Rank:3 kyu Honor:2,441

---
+++ JS/Reversed Strings - 8 kuy/Reversed Strings.md
@@ -0,0 +1,5 @@
+<div class="markdown prose max-w-none mb-8" id="description"><p>Complete the solution so that it reverses the string passed into it. </p>
+<pre><code>'world'  =&gt;  'dlrow'
+'word'   =&gt;  'drow'
+</code></pre>
+</div>
---
+++ JS/Reversed Strings - 8 kuy/Reversed Strings.test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+const solution = require('./Reversed Strings');
+
+describe('Basic tests', () => {
+    it('Testing for fixed tests', () => {
+        assert.strictEqual(solution('world'), 'dlrow');
+        assert.strictEqual(solution('hello'), 'olleh');
+        assert.strictEqual(solution(''), '');
+        assert.strictEqual(solution('h'), 'h');
+    });
+});
